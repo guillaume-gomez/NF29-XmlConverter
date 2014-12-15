cc4667f6598525016a5341000c99c8d7fff22681
guillaume-gomez <guillaume.gomez846@gmail.com>
2014-12-15 11:11:50 +0100

    add samples, change the main ant

---
+++ samples/sample.mm
@@ -0,0 +1,19 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1418378469636" ID="ID_1761352614" MODIFIED="1418378846033" TEXT="Compte rendu reunion">
+<node CREATED="1418378601388" ID="ID_316552916" MODIFIED="1418378852113" POSITION="left" TEXT="Avanc&#xe9; du travail">
+<node CREATED="1418378754221" ID="ID_378495707" MODIFIED="1418378796204" TEXT="Am&#xe9;lioration du rendement"/>
+<node CREATED="1418378777668" ID="ID_1029064924" MODIFIED="1418378790435" TEXT="Cr&#xe9;ation d&apos;un nouveau systeme de paiement"/>
+</node>
+<node CREATED="1418378647414" ID="ID_58016685" MODIFIED="1418378674936" POSITION="left" TEXT="R&#xe9;sultat courrant">
+<node CREATED="1418378826291" ID="ID_1497466150" MODIFIED="1418378837905" TEXT="70% du travail fait"/>
+</node>
+<node CREATED="1418378677920" ID="ID_92198961" MODIFIED="1418378859207" POSITION="left" TEXT="Rappel du travail &#xe0; realiser">
+<node CREATED="1418378863191" ID="ID_1997168106" MODIFIED="1418378890823" TEXT="Changement du precess de relation client">
+<node CREATED="1418378925585" ID="ID_1529753947" MODIFIED="1418378933336" TEXT="Formation"/>
+<node CREATED="1418378953204" ID="ID_1194109046" MODIFIED="1418378981259" TEXT="Sondage client&#xe8;le"/>
+</node>
+<node CREATED="1418378895031" ID="ID_412980386" MODIFIED="1418378920965" TEXT="Trouver de nouveaux client d&apos;ici la fin de l&apos;ann&#xe9;e"/>
+</node>
+</node>
+</map>
